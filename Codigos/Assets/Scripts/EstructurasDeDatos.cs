31fcf9d448d4f9a4149a99603bee4b66b55261aa
Jonathan Ivan <Jonathan.ivan1945@gmail.com>
2023-05-23 23:16:09 -0600

    Codes

---
+++ Codigos/Assets/Scripts/EstructurasDeDatos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstructurasDeDatos : MonoBehaviour
+{
+
+    static void Saludar()
+    {
+      
+        Debug.Log("There is superstition");
+    }
+
+
+
+
+    void Start()
+    {
+        Saludar();
+    }
+
+    void Update()
+    {
+        
+    }
+}
